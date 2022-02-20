45e1b28707c2ee9d4e946f34b23c7ccebe213be6
niuniu315 <454378439@qq.com>
2022-02-20 20:16:25 +0800

    初始化route

---
+++ src/components/Frank.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  我的第一个组件
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Frank'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Frank2.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Frank2</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Frank2'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

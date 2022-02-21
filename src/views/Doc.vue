bbb428f8c02a3fb3b1e381dd425be994f6df4abc
niuniu315 <454378439@qq.com>
2022-02-21 16:24:57 +0800

    加入菜单图片

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
   <div>
     <Topnav/>
     <div class="content">
-      <aside>
+      <aside v-if="menuVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -26,10 +26,15 @@
 
 <script lang="ts">
 import Topnav from '../components/Topnav.vue';
+import {inject, Ref} from 'vue';
 
 export default {
   name: 'Doc',
-  components: {Topnav}
+  components: {Topnav},
+  setup(){
+    const menuVisible = inject<Ref<boolean>>('menuVisible')
+    return{menuVisible}
+  }
 };
 </script>
 

762ebf15af9399357e56ab271f432659570211d1
niuniu315 <454378439@qq.com>
2022-02-23 00:07:27 +0800

    完善aside切换逻辑

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -31,28 +31,34 @@ import {inject, Ref} from 'vue';
 export default {
   name: 'Doc',
   components: {Topnav},
-  setup(){
-    const menuVisible = inject<Ref<boolean>>('menuVisible')
-    return{menuVisible}
+  setup() {
+    const menuVisible = inject<Ref<boolean>>('menuVisible');
+    return {menuVisible};
   }
 };
 </script>
 
 <style lang="scss" scoped>
-aside{
+aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  > h2{
+
+  > h2 {
     margin-bottom: 4px;
-  }> ol{
-  >li{
-    padding: 4px 0;
   }
-     }
+
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+  }
 }
 </style>

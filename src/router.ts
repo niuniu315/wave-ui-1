9367e339f6311e1b0df0b73035a344e168e953ab
niuniu315 <454378439@qq.com>
2022-02-23 22:04:24 +0800

    完成路由切换之后关闭aside

---
+++ src/router.ts
@@ -0,0 +1,26 @@
+import Home from './views/Home.vue';
+import Doc from './views/Doc.vue';
+import {createWebHashHistory, createRouter} from 'vue-router';
+import SwitchDemo from './components/SwitchDemo.vue'
+
+const history = createWebHashHistory();
+export const router = createRouter({
+  history: history,
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path:'/doc',
+      component: Doc,
+      children:[{
+        path:'switch',component:SwitchDemo
+      }]
+    }
+  ]
+});
+
+router.afterEach(() => {
+
+})

7e3629fb2d213324a0c4cd726f40ae6d89215edd
niuniu315 <454378439@qq.com>
2022-02-25 15:17:29 +0800

    修改switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -28,7 +28,7 @@ button {
   height: $h;
   width: $h*2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
@@ -41,14 +41,31 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
-  transition: left 250ms;
+  transition: all 250ms;
 }
 
 button.checked {
-  background: blue;
+  background: #1890ff;
 }
 
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+/* 取消默认有的边框线*/
+button:focus {
+  outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>

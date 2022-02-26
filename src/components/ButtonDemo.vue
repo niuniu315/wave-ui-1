4746820d996679d3d49ffa5319680959ea418e75
niuniu315 <454378439@qq.com>
2022-02-26 22:42:52 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,27 @@
 <template>
-<div>
-  button式例
-</div>
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button @click="onClick"
+            @focus="onClick"
+            @mouseover="onClick"
+    >你好
+    </Button>
+  </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
+
 export default {
-  name: 'ButtonDemo'
+  name: 'ButtonDemo',
+  components: {Button},
+  setup() {
+    const onClick = () => {
+      console.log('hi');
+    }
+    return {onClick}
+  }
 };
 </script>
 
---
+++ src/lib/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot/>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Button',
+  inheritAttrs: false,
+  props: {},
+  setup(props, context) {
+    const {size, ...rest} = context.attrs;
+    return{size, rest};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

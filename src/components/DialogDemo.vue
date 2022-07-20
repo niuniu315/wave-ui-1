0b6e4c90fa598ee7e54e98ffabdbc99d4af7c661
niuniu315 <454378439@qq.com>
2022-07-20 20:55:02 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,15 @@
 <template>
-<div>
-  Dialog式例
-</div>
+  <div>Dialog示例</div>
+  <h1>示例一</h1>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue';
+
 export default {
-  name: 'DialogDemo'
+  name: 'DialogDemo',
+  components: {Dialog}
 };
 </script>
 

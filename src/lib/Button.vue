d8fa32da8e0d61a829d3cf4ca1957d6a0b968ee7
niuniu315 <454378439@qq.com>
2022-07-20 11:03:59 +0800

    完成button css样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -9,7 +9,7 @@ export default {
   name: 'Button',
   props: {
     theme: {
-      type:String,
+      type: String,
       default: 'button'
     }
   },
@@ -17,5 +17,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+.gulu-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
 
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+
+  &:focus {
+    outline: none;
+  }
+//上面样式在firefox里不能用，需要加上下面这个样式
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
 </style>

216fedc28941905d05ec1a9174b79cd0cf752c4d
niuniu315 <454378439@qq.com>
2022-07-23 21:37:28 +0800

    创建文档路由

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,19 +3,31 @@
     <Topnav :toggleMenuButtonVisible="true" class="nav"/>
     <div class="content">
       <aside v-if="asideVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/start">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
-            <router-link to="/doc/switch">Switch开关</router-link>
+            <router-link to="/doc/switch">Switch组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/button">Button按钮</router-link>
+            <router-link to="/doc/button">Button组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/dialog">Dialog对话框</router-link>
+            <router-link to="/doc/dialog">Dialog组件</router-link>
           </li>
           <li>
-            <router-link to="/doc/tabs">Tabs标签页</router-link>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
           </li>
         </ol>
       </aside>
---
+++ src/views/Start.vue
@@ -0,0 +1,13 @@
+<template>
+<div>开始</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Start'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>

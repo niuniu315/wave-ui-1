5cec615a07b6e63399da3795e6ea4fb21dcc219f
niuniu315 <454378439@qq.com>
2022-07-24 16:22:11 +0800

    抽离SwitchDemo至Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,21 +1,34 @@
 <template>
-  <div>
-    <h1>Switch 组件示例 </h1>
-    <Demo :component="Switch1"/>
+  <div class="demo">
+    <h2>{{ component.__sourceCodeTitle }}</h2>
+    <div class="demo-component">
+      <component :is="component"/>
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')"/>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
-import Switch1 from './Demos/Switch1.vue';
-import Demo from './Demo.vue';
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css';
+import Button from '../lib/Button.vue';
 
+const Prism = (window as any).Prism;
 export default {
-  name: 'SwitchDemo',
-  components: {Demo, Switch1},
+  components: {Button},
+  props: {
+    component: Object
+  },
   setup() {
-    const bool = ref(false);
-    return {bool, Switch1};
+    return {
+      Prism
+    };
   }
 };
 </script>
